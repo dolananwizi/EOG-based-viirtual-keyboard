731e1dcda5c6f70dc67304a4aa69a1f52042f1a5
Unknown <dolananwizi@gmail.com>
2017-04-07 21:05:20 +0700

    calibration 2
    determining sliding window length (peak-to-peak)

---
+++ detwindow.m
@@ -0,0 +1,38 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% calibration 2
+% Algoritm for determining the window length
+% update 1.0.3
+% by Arrijal Hanif - 2017
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function windowlength = detwindow(data_,fs,thvalue_)
+
+data = data_;
+thvalue = thvalue_;
+
+lendata = length(data);
+longdata = lendata/fs; % in seconds
+nn = longdata/5;
+
+firstdt = 1;
+lastdt = 1000;
+j =  1;
+
+for i = 1 : nn
+    [yh,xh] = max(data_(firstdt:lastdt));
+    [yl,xl] = min(data_(firstdt:lastdt));
+    
+    if(abs(yh) > thvalue && abs(yl) > thvalue)
+        windowlength_(j) = abs(xl-xh);
+        j = j + 1;
+    end
+    
+    firstdt = firstdt + 1000;
+    lastdt = lastdt + 1000;
+end
+
+% version 1
+windowlength = (min(windowlength_)/fs) - 0.1;
+windowlength = (round(windowlength*100))/100;
+
+end
