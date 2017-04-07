e5a3b35524bd957d7f4a1802abc33b73fc7230f2
Unknown <dolananwizi@gmail.com>
2017-04-07 21:10:56 +0700

    Editing FEX
    become : 1. real value and 0
    2. -1 0 1

---
+++ thresholdfex.m
@@ -0,0 +1,63 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% thresholding the data bar fex
+% 
+% By Arrijal Hanif - 2017
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+function [newratav,newratah] = thresholdfex(ratav,ratah,thvalue,option)
+
+numwindow = length(ratah);
+
+if(option == 1)
+%     real value
+    for i=1:numwindow
+%     vertical
+        if(ratav(i) >= thvalue || ratav(i) <= -thvalue)
+            ratav(i) = ratav(i);
+        else
+            ratav(i) = 0;
+        end
+
+    %     horizontal
+        if(ratah(i) >= thvalue || ratah(i) <= -thvalue)
+            ratah(i) = ratah(i);
+        else
+            ratah(i) = 0;
+        end
+    end
+elseif(option == 2)
+%     -1 0 1
+    for i=1:numwindow
+    %     vertical
+        if(ratav(i) >= thvalue)
+%             ratav(i) = 1;
+            if(ratav(i) > thvalue*6)
+                ratav(i) = 0;
+            else
+                ratav(i) = 1; 
+            end
+        elseif(ratav(i) <= -thvalue)
+%             ratav(i) = -1;
+            if(ratav(i) < -thvalue*6)
+                ratav(i) = 0;
+            else
+                ratav(i) = -1;
+            end
+        else
+            ratav(i) = 0;
+        end
+
+    %     horizontal
+        if(ratah(i) >= thvalue)
+            ratah(i) = 1;
+        elseif(ratah(i) <= -thvalue)
+            ratah(i) = -1;
+        else
+            ratah(i) = 0;
+        end
+    end
+end
+
+newratav = ratav;
+newratah = ratah;
